ee0bf405a00d235f912c3ff254803f75af0a11a9
juampapo546 <juampicarosi@gmail.com>
2021-04-26 09:50:17 -0300

    Fix "course not finished " error
    * Also add README.md and some feedback

---
+++ README.md
@@ -0,0 +1,13 @@
+# Como usar?
+
+1. Clonar el repositorio e instalar node
+
+```
+git clone https://github.com/juampapo546/regsitro && cd regsitro && npm i
+```
+
+2. Ejecuta pasando como parametros tu email y contraseña
+
+```
+npm start email contraseña
+```
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const puppeteer = require("puppeteer");
 (async () => {
+  const EMAIL = process.argv[2];
+  const PASSWORD = process.argv[3];
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(
     "https://curso.seguridadvial.gob.ar/ansv/index.php/registracion#"
   );
   await page.click(".btn-info");
-  await page.$eval("#login-email", (el) => (el.value = ""));
-  await page.$eval("#login-password", (el) => (el.value = ""));
+  await page.$eval("#login-email", (el, EMAIL) => (el.value = EMAIL), EMAIL);
+  await page.$eval(
+    "#login-password",
+    (el, PASSWORD) => (el.value = PASSWORD),
+    PASSWORD
+  );
   await page.click("#btn-ingresar");
   setTimeout(async () => {
     for (let i = 1; i <= 330; i++) {
@@ -15,5 +21,21 @@ const puppeteer = require("puppeteer");
         `https://curso.seguridadvial.gob.ar/ansv/index.php/capitulos/ver_subcapitulo/${i}`
       );
     }
+    await page.goto(
+      "https://curso.seguridadvial.gob.ar/ansv/index.php/cursos/finalizado/1"
+    );
+    await page
+      .goto(
+        "https://curso.seguridadvial.gob.ar/ansv/index.php/cursos/finalizado/3"
+      )
+      .then(() => {
+        console.log(
+          "Cursos terminados ! \n",
+          "Podes descargar los certificados en los siguientes links: \n",
+          "Auto: https://curso.seguridadvial.gob.ar/ansv/index.php/cursos/descargar_pdf/1 \n",
+          "Moto: https://curso.seguridadvial.gob.ar/ansv/index.php/cursos/descargar_pdf/3"
+        );
+        browser.close();
+      });
   }, 100);
 })();
