41da387390629a67efebf692e3fc09baa7c89a57
juampapo546 <juampicarosi@gmail.com>
2021-04-26 09:14:12 -0300

    init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const puppeteer = require("puppeteer");
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(
+    "https://curso.seguridadvial.gob.ar/ansv/index.php/registracion#"
+  );
+  await page.click(".btn-info");
+  await page.$eval("#login-email", (el) => (el.value = ""));
+  await page.$eval("#login-password", (el) => (el.value = ""));
+  await page.click("#btn-ingresar");
+  setTimeout(async () => {
+    for (let i = 320; i <= 330; i++) {
+      await page.goto(
+        `https://curso.seguridadvial.gob.ar/ansv/index.php/capitulos/ver_subcapitulo/${i}`
+      );
+    }
+  }, 100);
+})();
